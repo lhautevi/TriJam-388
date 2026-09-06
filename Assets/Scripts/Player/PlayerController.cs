0ff3d810e4544519ff4a349ae8b5c2f51abbf294
lhautevi <95317729+lhautevi@users.noreply.github.com>
2026-09-06 17:43:59 +0200

    player and ground base

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    
+    private Rigidbody2D rigidbody;
+    private RaycastHit2D _hit;
+
+    public float speed;
+    public float jumpForce;
+    public bool isGrounded ;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Is grounded ?
+        _hit = Physics2D.Raycast(transform.position, Vector2.down, 1.1f);
+        if (_hit)
+        {
+            UnityEngine.Debug.Log("tag" + _hit.collider.name);
+            if (_hit.collider.gameObject.CompareTag("Ground"))
+            {
+                UnityEngine.Debug.LogWarning("isGrounded");
+                isGrounded = true;
+            }
+        }
+        //deplacement
+        float dir = Input.GetAxisRaw("Horizontal");
+        rigidbody.linearVelocityX = dir * speed;
+
+        //Saut
+        if(isGrounded && Input.GetButtonDown("Jump"))
+        {
+            rigidbody.linearVelocityY = jumpForce;
+        }
+
+        
+    }
+}
